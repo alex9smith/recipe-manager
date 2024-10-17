e6378a6584d25cb5768b8b4b3c4506bbbe6b3020
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2024-10-17 16:37:27 +0100

    Fill out the recipe data model
    These fields should be enough for the MVP and dataclasses make it
    easy to add more in the future if we need to.

--- backend/models/recipe.py
+++ backend/models/recipe.py
@@ -1,6 +1,35 @@
 from dataclasses import dataclass
+from enum import Enum
+
+from typing import List
+
+
+class SourceType(Enum):
+    BOOK = 1
+    WEBSITE = 2
+
+
+@dataclass
+class Source:
+    pass
+
+
+@dataclass
+class Website(Source):
+    address: str
+    type: SourceType = SourceType.WEBSITE
+
+
+@dataclass
+class Book(Source):
+    title: str
+    page: int
+    type: SourceType = SourceType.BOOK
 
 
 @dataclass
 class Recipe:
     name: str
+    source: Source
+    ingredients: List[str]
+    tags: List[str]
--- test/test_backend/models/test_recipe.py
+++ test/test_backend/models/test_recipe.py
@@ -1,10 +1,12 @@
 from sys import path
 
 path.append("../../../backend")
-from backend.models.recipe import Recipe
+from backend.models.recipe import Recipe, Book
+
+book = Book(title="title", page=1)
 
 
 class TestRecipe:
     def test_recipe(self):
-        recipe = Recipe(name="A recipe")
+        recipe = Recipe(name="A recipe", source=book, ingredients=[], tags=[])
         assert recipe.name == "A recipe"
