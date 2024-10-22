bd160ed812334dc20fbe4ff9169a403d2ea8e782
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2024-10-22 08:58:35 +0100

    Correct get handler logger name

--- backend/handlers/recipes/get.py
+++ backend/handlers/recipes/get.py
@@ -5,7 +5,7 @@ from aws_lambda_powertools.utilities.typing import LambdaContext
 from backend.models.recipe import Recipe
 from backend.services.logger import get_logger
 
-logger = get_logger("create-recipe")
+logger = get_logger("recipes-get")
 
 
 @logger.inject_lambda_context
