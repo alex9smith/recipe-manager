0b0f9d9a6faad12362ad77c3aee1fd4ac0aec5f2
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2024-10-23 20:11:29 +0100

    Use lambda invoke ARNs in API GW integrations

--- infra/api.tf
+++ infra/api.tf
@@ -7,7 +7,7 @@ resource "aws_apigatewayv2_integration" "recipes_get" {
   api_id             = aws_apigatewayv2_api.recipe_manager_api.id
   integration_type   = "AWS_PROXY"
   integration_method = "POST"
-  integration_uri    = aws_lambda_function.recipes_get
+  integration_uri    = aws_lambda_function.recipes_get.invoke_arn
 }
 
 resource "aws_apigatewayv2_route" "recipes_get" {
@@ -20,7 +20,7 @@ resource "aws_apigatewayv2_integration" "recipes_post" {
   api_id             = aws_apigatewayv2_api.recipe_manager_api.id
   integration_type   = "AWS_PROXY"
   integration_method = "POST"
-  integration_uri    = aws_lambda_function.recipes_post
+  integration_uri    = aws_lambda_function.recipes_post.invoke_arn
 }
 
 resource "aws_apigatewayv2_route" "recipes_post" {
