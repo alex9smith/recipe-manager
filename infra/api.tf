2c8b190d7e59f2a784fdc5c89934ca6c14772900
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2024-10-23 19:57:40 +0100

    Set up first two routes in API Gateway
    I'm not sure the integration parameters are completely correct here,
    particularly the integration method.

---
+++ infra/api.tf
@@ -0,0 +1,30 @@
+resource "aws_apigatewayv2_api" "recipe_manager_api" {
+  name          = "${var.application_name}_api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "recipes_get" {
+  api_id             = aws_apigatewayv2_api.recipe_manager_api.id
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+  integration_uri    = aws_lambda_function.recipes_get
+}
+
+resource "aws_apigatewayv2_route" "recipes_get" {
+  api_id    = aws_apigatewayv2_api.recipe_manager_api.id
+  route_key = "GET /api/v1/recipes"
+  target    = "integrations/${aws_apigatewayv2_integration.recipes_get.id}"
+}
+
+resource "aws_apigatewayv2_integration" "recipes_post" {
+  api_id             = aws_apigatewayv2_api.recipe_manager_api.id
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+  integration_uri    = aws_lambda_function.recipes_post
+}
+
+resource "aws_apigatewayv2_route" "recipes_post" {
+  api_id    = aws_apigatewayv2_api.recipe_manager_api.id
+  route_key = "POST /api/v1/recipes"
+  target    = "integrations/${aws_apigatewayv2_integration.recipes_post.id}"
+}
