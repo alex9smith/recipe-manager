3357bb8b5f82e6305e772a7888b193aa1543b8ea
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2024-10-24 08:35:19 +0100

    Specify v2.0 payload version from API Gateway
    This format is what the lambdas are expecting, but it defaults to 1.0

--- infra/api.tf
+++ infra/api.tf
@@ -4,10 +4,10 @@ resource "aws_apigatewayv2_api" "recipe_manager_api" {
 }
 
 resource "aws_apigatewayv2_integration" "recipes_get" {
-  api_id             = aws_apigatewayv2_api.recipe_manager_api.id
-  integration_type   = "AWS_PROXY"
-  integration_method = "POST"
-  integration_uri    = aws_lambda_function.recipes_get.invoke_arn
+  api_id                 = aws_apigatewayv2_api.recipe_manager_api.id
+  integration_type       = "AWS_PROXY"
+  payload_format_version = "2.0"
+  integration_uri        = aws_lambda_function.recipes_get.invoke_arn
 }
 
 resource "aws_apigatewayv2_route" "recipes_get" {
@@ -17,10 +17,10 @@ resource "aws_apigatewayv2_route" "recipes_get" {
 }
 
 resource "aws_apigatewayv2_integration" "recipes_post" {
-  api_id             = aws_apigatewayv2_api.recipe_manager_api.id
-  integration_type   = "AWS_PROXY"
-  integration_method = "POST"
-  integration_uri    = aws_lambda_function.recipes_post.invoke_arn
+  api_id                 = aws_apigatewayv2_api.recipe_manager_api.id
+  integration_type       = "AWS_PROXY"
+  payload_format_version = "2.0"
+  integration_uri        = aws_lambda_function.recipes_post.invoke_arn
 }
 
 resource "aws_apigatewayv2_route" "recipes_post" {
