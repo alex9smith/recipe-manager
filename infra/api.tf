763be748f78ea8a477ad6c4331e0378152040b3f
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2024-10-24 08:47:08 +0100

    Add a default stage
    This is required to use the base API Gateway URL. Set `auto_deploy` to
    true so we don't need to trigger a deployment each time.

--- infra/api.tf
+++ infra/api.tf
@@ -3,6 +3,12 @@ resource "aws_apigatewayv2_api" "recipe_manager_api" {
   protocol_type = "HTTP"
 }
 
+resource "aws_apigatewayv2_stage" "default_stage" {
+  api_id      = aws_apigatewayv2_api.recipe_manager_api.id
+  name        = "$default"
+  auto_deploy = "true"
+}
+
 resource "aws_apigatewayv2_integration" "recipes_get" {
   api_id                 = aws_apigatewayv2_api.recipe_manager_api.id
   integration_type       = "AWS_PROXY"
