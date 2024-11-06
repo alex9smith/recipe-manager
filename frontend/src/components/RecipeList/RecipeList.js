d956c0ac3696301a6a27148b3168546cdeac1700
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2024-11-06 10:29:14 +0000

    Fix position of elements on plan page
    This means the plan page probably won't work on mobile but that's ok -
    the design also doesn't work on mobile and we do all our meal planning
    on laptops anyway.

--- frontend/src/components/RecipeList/RecipeList.js
+++ frontend/src/components/RecipeList/RecipeList.js
@@ -1,5 +1,7 @@
 import RecipeListItem from "../RecipeListItem/RecipeListItem";
 
+import "./RecipeList.css";
+
 function RecipeList({ recipes }) {
   const items = recipes.map((recipe) => (
     <RecipeListItem recipe={recipe} key={recipe.id} />
