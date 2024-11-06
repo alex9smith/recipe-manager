4be7fad760b574a0598bcbbce96b00e0acea1663
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2024-11-06 14:10:38 +0000

    Style the popup and allow user to save notes

--- frontend/src/components/PlanPopup/PlanPopup.js
+++ frontend/src/components/PlanPopup/PlanPopup.js
@@ -5,22 +5,34 @@ function PlanPopup({ date, plan, setPlan, recipe }) {
     const newPlan = { ...plan };
     delete newPlan[date];
     setPlan(newPlan);
-    e.target.parentElement.classList.remove("active");
+    e.target.parentElement.parentElement.classList.remove("active");
   }
 
   function closePopup(e) {
-    e.target.parentElement.classList.remove("active");
+    e.target.parentElement.parentElement.classList.remove("active");
+  }
+
+  function updateNotes(e) {
+    const newPlan = { ...plan };
+    newPlan[date].notes = e.target.value;
+    setPlan(newPlan);
   }
 
   return (
     <div className="plan-popup">
-      <div>{recipe.name}</div>
-      <button type="button" onClick={clearDay}>
-        Clear
-      </button>
-      <button type="button" onClick={closePopup}>
-        Close
-      </button>
+      <div className="plan-popup-overlay"></div>
+      <div className="plan-popup-content">
+        <div>{recipe.name}</div>
+        <div className="notes">
+          <textarea value={plan[date].notes} onChange={updateNotes} />
+        </div>
+        <button type="button" onClick={clearDay}>
+          Clear recipe
+        </button>
+        <button type="button" onClick={closePopup}>
+          Close
+        </button>
+      </div>
     </div>
   );
 }
