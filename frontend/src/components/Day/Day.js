a56b0578c7200eb66d63b3359e0bde3bf02ebd36
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2024-11-06 11:22:59 +0000

    Allow users to drag recipes onto the calendar plan

--- frontend/src/components/Day/Day.js
+++ frontend/src/components/Day/Day.js
@@ -1,6 +1,23 @@
+function onDragOver(e) {
+  e.preventDefault();
+  e.dataTransfer.effectAllowed = "copy";
+}
+
+function onDrop(e) {
+  e.preventDefault();
+  const id = e.dataTransfer.getData("text/id");
+  const name = e.dataTransfer.getData("text/name");
+  e.target.textContent = name;
+}
+
 function Day({ date, state }) {
   return (
-    <div className={"day " + state} key={date}>
+    <div
+      className={"day " + state}
+      key={date}
+      onDrop={onDrop}
+      onDragOver={onDragOver}
+    >
       {date}
     </div>
   );
--- frontend/src/components/RecipeListItem/RecipeListItem.js
+++ frontend/src/components/RecipeListItem/RecipeListItem.js
@@ -1,10 +1,17 @@
 import { Link } from "react-router-dom";
 
+function onDragStart(e) {
+  e.dataTransfer.setData("text/id", e.target.id);
+  e.dataTransfer.setData("text/name", e.target.textContent);
+}
+
 function RecipeListItem({ recipe }) {
   const url = `/recipes/${recipe.id}`;
   return (
     <li>
-      <Link to={url}>{recipe.name}</Link>
+      <Link to={url} draggable="true" onDragStart={onDragStart} id={recipe.id}>
+        {recipe.name}
+      </Link>
     </li>
   );
 }
