1cbf2330bf9d7b338085cc3c8f6a062b8c65abd2
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2024-11-18 19:23:30 +0000

    Start styling the recipes in the list

--- frontend/src/components/RecipeListItem/RecipeListItem.js
+++ frontend/src/components/RecipeListItem/RecipeListItem.js
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 
+import "./RecipeListItem.css";
+
 function onDragStart(e) {
   e.dataTransfer.setData("text/id", e.target.id);
 }
@@ -7,7 +9,7 @@ function onDragStart(e) {
 function RecipeListItem({ recipe }) {
   const url = `/recipes/${recipe.id}`;
   return (
-    <li>
+    <li className="recipe-list-item">
       <Link to={url} draggable="true" onDragStart={onDragStart} id={recipe.id}>
         {recipe.name}
       </Link>
