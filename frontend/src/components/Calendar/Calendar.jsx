d4077b4e7fecc7795028c9d464fa655656e79b93
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2024-11-19 19:59:32 +0000

    Use Date objects to populate the calendar

--- frontend/src/components/Calendar/Calendar.jsx
+++ frontend/src/components/Calendar/Calendar.jsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
+import { Box } from "@primer/react";
 import Day from "../Day/Day";
+import MonthSelector from "../MonthSelector/MonthSelector";
 
 import "./Calendar.css";
 
 function calculateState(day, today) {
-  if (day === today) {
+  if (
+    day.getFullYear() === today.getFullYear() &&
+    day.getMonth() === today.getMonth() &&
+    day.getDate() === today.getDate()
+  ) {
     return "today";
   }
   if (day < today) {
@@ -25,26 +31,36 @@ function firstDayOfTheMonth() {
 
 function Calendar({ plan, setPlan, recipes }) {
   const [startDate, setStartDate] = useState(firstDayOfTheMonth());
+  const today = new Date();
   const days = [];
   for (
     let i = 1;
     i <= daysInMonth(startDate.getFullYear(), startDate.getMonth());
     i++
   ) {
-    const date = new Date();
+    const date = new Date(
+      startDate.getFullYear(),
+      startDate.getMonth(),
+      startDate.getDate()
+    );
     date.setDate(i);
     days.push(
       <Day
         key={i}
         date={date}
-        state={calculateState(i, startDate.getDate())}
+        state={calculateState(date, today)}
         plan={plan}
         setPlan={setPlan}
         recipes={recipes}
       />
     );
   }
-  return <div className="calendar">{days}</div>;
+  return (
+    <Box>
+      <MonthSelector startDate={startDate} setStartDate={setStartDate} />
+      <div className="calendar">{days}</div>
+    </Box>
+  );
 }
 
 export default Calendar;
---
+++ frontend/src/components/MonthSelector/MonthSelector.jsx
@@ -0,0 +1,7 @@
+import { Box } from "@primer/react";
+
+function MonthSelector({ startDate, setStartDate }) {
+  return <Box>Month</Box>;
+}
+
+export default MonthSelector;
