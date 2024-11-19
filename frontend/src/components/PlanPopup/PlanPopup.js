43c2969e3e4900137ed51fbd347d68df8a7eb1f8
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2024-11-19 16:16:40 +0000

    Display recipe information on popup

--- frontend/src/components/PlanPopup/PlanPopup.js
+++ frontend/src/components/PlanPopup/PlanPopup.js
@@ -1,4 +1,6 @@
 import { Box, Dialog, Textarea } from "@primer/react";
+import Source from "../Source/Source";
+import Ingredients from "../Ingredients/Ingredients";
 
 function PlanPopup({ date, plan, setPlan, togglePopup }) {
   function clearDay(e) {
@@ -34,6 +36,13 @@ function PlanPopup({ date, plan, setPlan, togglePopup }) {
       ]}
     >
       <Box>{daysPlan.recipe?.name}</Box>
+      <br></br>
+      {daysPlan.recipe.source && (
+        <Box>
+          <Source source={daysPlan.recipe.source} />
+          <Ingredients recipe={daysPlan.recipe} />
+        </Box>
+      )}
       <Textarea value={daysPlan.notes} onChange={updateNotes} />
     </Dialog>
   );
