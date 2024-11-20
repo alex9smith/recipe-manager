b50ecb805fcf1cccf0eeabf437400b9d8491fb52
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2024-11-20 08:50:53 +0000

    Allow users to switch between calendar months

--- frontend/src/components/MonthSelector/MonthSelector.jsx
+++ frontend/src/components/MonthSelector/MonthSelector.jsx
@@ -1,7 +1,35 @@
 import { Box } from "@primer/react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@primer/octicons-react";
 
 function MonthSelector({ startDate, setStartDate }) {
-  return <Box>Month</Box>;
+  function nextMonth() {
+    const newStartDate = new Date(
+      startDate.getFullYear(),
+      startDate.getMonth() + 1,
+      startDate.getDate()
+    );
+    setStartDate(newStartDate);
+  }
+
+  function previousMonth() {
+    const newStartDate = new Date(
+      startDate.getFullYear(),
+      startDate.getMonth() - 1,
+      startDate.getDate()
+    );
+    setStartDate(newStartDate);
+  }
+  return (
+    <Box sx={{ mb: 1 }}>
+      {startDate.toLocaleString("en-gb", { month: "long", year: "numeric" })}
+      <span onClick={previousMonth}>
+        <ChevronLeftIcon size={24} />
+      </span>
+      <span onClick={nextMonth}>
+        <ChevronRightIcon size={24} />
+      </span>
+    </Box>
+  );
 }
 
 export default MonthSelector;
