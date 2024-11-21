0e7218f08c2076a748430bf892b7c23c1a2ed693
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2024-11-21 22:18:40 +0000

    Add debug logging

--- frontend/src/services/google.js
+++ frontend/src/services/google.js
@@ -1,4 +1,6 @@
 export async function fetchUserProfile(tokenResponse) {
+  console.log("token response:");
+  console.log(tokenResponse);
   const response = await fetch(
     "https://www.googleapis.com/oauth2/v3/userinfo",
     { headers: { Authorization: `Bearer ${tokenResponse.access_token}` } }
