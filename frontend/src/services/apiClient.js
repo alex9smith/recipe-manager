b80d94b08fecb9ef95eb00eeb1f3f71f61744bf1
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2024-11-21 12:13:28 +0000

    Cache the recipe list data in local storage

--- frontend/src/services/apiClient.js
+++ frontend/src/services/apiClient.js
@@ -1,9 +1,17 @@
 import { getApiBaseUrl } from "../constants";
 
+// 5 minutes in milliseconds
+const EXPIRES_IN = 5 * 60 * 1000;
+
+function hasExpired(timestamp) {
+  return timestamp <= Date.now() - EXPIRES_IN;
+}
+
 class ApiClient {
   constructor(baseUrl) {
     this.baseUrl = baseUrl;
     this.prefix = "/api/v1";
+    this.storage = window.sessionStorage;
   }
 
   async #getUrl(route) {
@@ -15,8 +23,36 @@ class ApiClient {
     return await response.json();
   }
 
+  #cacheRecipes(recipes) {
+    this.storage.setItem("recipes-saved-at", Date.now());
+    this.storage.setItem("recipes", JSON.stringify(recipes));
+  }
+
+  #getCachedRecipes() {
+    const savedAt = this.storage.getItem("recipes-saved-at");
+    const recipes = this.storage.getItem("recipes");
+
+    // if we don't have both items
+    if (!(savedAt && recipes)) {
+      return null;
+    }
+
+    if (hasExpired(parseInt(savedAt))) {
+      return null;
+    }
+
+    return JSON.parse(recipes);
+  }
+
   async getAllRecipes() {
-    return await this.#getUrl("/recipes");
+    const cached = this.#getCachedRecipes();
+    if (cached) {
+      return cached;
+    }
+    // otherwise fetch the recipes and cache the result
+    const recipes = await this.#getUrl("/recipes");
+    this.#cacheRecipes(recipes);
+    return recipes;
   }
 
   async getRecipe(id) {
